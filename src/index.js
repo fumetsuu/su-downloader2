d0f2a2f36d47d7da9c5124c5f6bb53b05c52e782
Joseph Surin <joseph.surin@gmail.com>
2018-04-05 18:24:37 +0800

    export sudownloader

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import { initiateDownload } from './initiateDownload'
 import { startDownload } from './startDownload'
 import { sudPath, partialPath } from './Utils'
+const suDownloader = require('./suDownloader')
 
 module.exports = {
+	suDownloader,
 	initiateDownload,
 	startDownload,
 	sudPath,
